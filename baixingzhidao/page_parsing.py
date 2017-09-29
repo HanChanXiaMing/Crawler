14726805b0620e3ad63cc277fffee4b011b4238c
HanChanXiaMing <31791527+HanChanXiaMing@users.noreply.github.com>
2017-09-29 11:42:43 +0800

    Update page_parsing.py

--- baixingzhidao/page_parsing.py
+++ baixingzhidao/page_parsing.py
@@ -3,9 +3,9 @@ import requests
 import time
 import pymongo
 
-client = pymongo.MongoClient('localhost',27017)
-cheshi = client['ceshi']    # 创建一个库
-url_list = cheshi['url_list3']  # 创建一个表
+client = pymongo.MongoClient('localhost',27017) # 激活
+cheshi = client['cheshi']    # 创建一个库
+url_list = cheshi['url_list3']  
 item_info = cheshi['item_info4']
 url_host = 'http://communityaction.org.cn'
 
@@ -17,28 +17,39 @@ def get_links_from(channel,pages):  # channel 是类的html   pages 是当前类
     wb_data = requests.get(list_view)
     time.sleep(3)   # 延迟时间
     soup = BeautifulSoup(wb_data.text,'lxml')
-    for link in soup.select('a.dwqa-title'):
-        url = url_host + link.get('href')
-        url_list.insert_one({'url':url})
-        print(url)
+    if soup.find('footer','dwqa-footer-meta'):    # 判断当前页有无信息
+        for link in soup.select('a.dwqa-title'):
+            url = url_host + link.get('href')
+            url_list.insert_one({'url':url})
+            print(url)
+    else:
+        pass
+        # Nothing !
 
 # spider 2
 # 获取各页里的信息
 def get_item_info(url):
         wb_data = requests.get(url)
         soup = BeautifulSoup(wb_data.text,'lxml')
-        title = soup.select('h1.entry-title')[0].text               # 标题
-        data = soup.select('div.dwqa-meta span.dwqa-date')[0].text  # 提问时间
-        #Answer_time = soup.select('header span.dwqa-date a').text        # 未被采用回答时间
-        #if soup.find('div','acceptAnswer'):                         # 回答是否采用
-        best_time = soup.select('div.acceptAnswer div span:nth-of-type(2)')[0].text # 采用回答时间
-
-        #item_info.insert_one({'title':title,'data:'data,'best_time:'best_time})
-        print({'title':title,'data:'data,'best_time:'best_time})
-
-get_item_info('http://communityaction.org.cn/question/265604.html')
-get_links_from('http://communityaction.org.cn/page-100',1)
-
+        titles = soup.select('h1.entry-title')               # 标题
+        datas = soup.select('div.dwqa-meta span.dwqa-date')  # 提问时间
+        Answer_times = soup.select('header div span.dwqa-date a')        # 未被采用回答时间
+        if soup.find('div','acceptAnswer'):                  # 回答是否被采用
+            best_times = soup.select('div.acceptAnswer div span:nth-of-type(2)') # 采用回答时间
+        else:
+            best_times = None
+        print(titles,datas,Answer_times,best_times)
+        #！ Answer_times 输出时只有一个 未解决 75-78
+        #for title,data,Answer_time,best_time in zip(titles,datas,Answer_times,best_times):
+        #    text = {
+        #        'tiele' :title.get_text(),
+        #        'data'  :data.get_text(),
+        #        'Answer_time':Answer_time.get_text(),
+        #        'best_time' :best_time.get_text()
+        #    }
+        #    print(text)
+#get_links_from('http://communityaction.org.cn/page-100',1)
+get_item_info('http://communityaction.org.cn/question/265922.html')
 '''
 http://communityaction.org.cn/question/265604.html
 http://communityaction.org.cn/question/265573.html
@@ -61,4 +72,9 @@ http://communityaction.org.cn/question/264121.html
 http://communityaction.org.cn/question/263397.html
 http://communityaction.org.cn/question/263050.html
 
+        # answer- > header:nth-child(1) > div > span.dwqa-date > a
+        # answer- > header:nth-child(3) > div > span.dwqa-date > a
+        # answer- > header:nth-child(5) > div > span.dwqa-date > a
+        # answer- > header:nth-child(7) > div > span.dwqa-date > a
+        #answer- > div.acceptAnswer > div > span.accept
 '''
